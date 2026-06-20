b8885253bee2d5cfdae48dd8d2150b2d48582e9f
AGEN233 <AGEN233@hotmail.com>
2026-06-20 23:15:08 +0800

    refactor: manage ST7735 runtime rotation state

--- DisplayDriver/display7735/display7735.h
+++ DisplayDriver/display7735/display7735.h
@@ -9,10 +9,38 @@ extern "C" {
 #include "stdbool.h"
 #include "stdint.h"
 #include "esp_err.h"
+#include "esp_lcd_panel_io.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 typedef void (*display_st7735_flush_done_cb_t)(void);
+
+typedef enum {
+    DISPLAY_ST7735_ROTATION_0 = 0,
+    DISPLAY_ST7735_ROTATION_90,
+    DISPLAY_ST7735_ROTATION_180,
+    DISPLAY_ST7735_ROTATION_270,
+} display_st7735_rotation_et;
+
+typedef struct {
+    uint16_t width;
+    uint16_t height;
+    display_st7735_rotation_et rotation;
+
+    bool initialized;
+    display_st7735_flush_done_cb_t flush_done_cb;
+
+    esp_lcd_panel_io_handle_t panel_io;
+    TaskHandle_t test_task_handle;
+} display_st7735_runtime_st;
+
 esp_err_t display_st7735_init(display_st7735_flush_done_cb_t cb);
 esp_err_t display_st7735_set_backlight(bool enabled);
+esp_err_t display_st7735_set_rotation(display_st7735_rotation_et rotation);
+display_st7735_rotation_et display_st7735_get_rotation(void);
+
+uint16_t display_st7735_get_width(void);
+uint16_t display_st7735_get_height(void);
 esp_err_t display_st7735_flush(uint16_t x_start, uint16_t y_start, uint16_t x_end, uint16_t y_end, const void *rgb565_data);
 esp_err_t display_st7735_test(void);
 #endif

eee83db42c6338d062ffc505f0c813c3a7b3b421
AGEN233 <AGEN233@hotmail.com>
2026-06-20 15:52:07 +0800

    [0.1.1]refactor-> 模块化组件 CMake 配置

--- agcore.h
+++ agcore.h
@@ -11,7 +11,7 @@
 
 #define AGCORE_VERSION_MAJOR    0
 #define AGCORE_VERSION_MINOR    1
-#define AGCORE_VERSION_PATCH    0
+#define AGCORE_VERSION_PATCH    1
 
 #define AGCORE_VERSION       0x01
 #define AGCORE_ID            0x01

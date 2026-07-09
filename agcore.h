9e40cfd1cc8f3f3f240ca10de395a0c4ba290492
AGEN233 <AGEN233@hotmail.com>
2026-07-09 23:57:04 +0800

    [0.4.1]refactor-> 统一显示驱动目录结构

--- agcore.h
+++ agcore.h
@@ -11,7 +11,7 @@
 
 #define AGCORE_VERSION_MAJOR    0
 #define AGCORE_VERSION_MINOR    4
-#define AGCORE_VERSION_PATCH    0
+#define AGCORE_VERSION_PATCH    1
 
 #define AGCORE_VERSION       0x02
 #define AGCORE_ID            0x01
--- display_driver/display_driver.h
+++ display_driver/display_driver.h
@@ -1,9 +1,36 @@
 #ifndef __DISPLAY_DRIVER_H__
 #define __DISPLAY_DRIVER_H__
+
 #include "sdkconfig.h"
+#include "stdbool.h"
+#include "stdint.h"
+#include "esp_err.h"
 
 #if defined(CONFIG_DISPLAY_DRIVER_ENABLE) && defined(CONFIG_DISPLAY_IC_ST7735)
-#include "display7735/display7735.h"
+#define DISPLAY_DRIVER_WIDTH  CONFIG_DISPLAY_ST7735_WIDTH
+#define DISPLAY_DRIVER_HEIGHT CONFIG_DISPLAY_ST7735_HEIGHT
+#elif defined(CONFIG_DISPLAY_DRIVER_ENABLE) && defined(CONFIG_DISPLAY_IC_ST7789)
+#define DISPLAY_DRIVER_WIDTH  CONFIG_DISPLAY_ST7789_WIDTH
+#define DISPLAY_DRIVER_HEIGHT CONFIG_DISPLAY_ST7789_HEIGHT
 #endif
 
-#endif
+typedef void (*display_driver_flush_done_cb_t)(void);
+
+typedef enum {
+    DISPLAY_DRIVER_ROTATION_0 = 0,
+    DISPLAY_DRIVER_ROTATION_90,
+    DISPLAY_DRIVER_ROTATION_180,
+    DISPLAY_DRIVER_ROTATION_270,
+} display_driver_rotation_et;
+
+esp_err_t display_driver_init(display_driver_flush_done_cb_t cb);
+esp_err_t display_driver_set_backlight(bool enabled);
+esp_err_t display_driver_set_rotation(display_driver_rotation_et rotation);
+display_driver_rotation_et display_driver_get_rotation(void);
+
+uint16_t display_driver_get_width(void);
+uint16_t display_driver_get_height(void);
+esp_err_t display_driver_flush(uint16_t x_start, uint16_t y_start, uint16_t x_end, uint16_t y_end, const void *rgb565_data);
+esp_err_t display_driver_test(void);
+
+#endif /* __DISPLAY_DRIVER_H__ */
